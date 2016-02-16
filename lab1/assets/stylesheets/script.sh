9ac4e903f58111817d787eb1414223bbd6d2cede
Maciej Posłuszny <xmachiei@gmail.com>
2016-02-16 22:32:42 +0100

    created bash scripts to generate files for next exercises

---
+++ lab1/assets/stylesheets/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for f in {4..10}
+do
+    echo "// out: ../css/zad_0$f.css, compress: true" > "zad_0$f.less"
+done
---
+++ lab1/pages/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for f in {4..10}
+do
+    echo hello > "zad_0$f.html"
+done
