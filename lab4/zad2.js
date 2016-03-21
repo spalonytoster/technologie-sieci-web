e0c78908c1096aa9c221f95fb4ad7720837d5b23
spalonytoster <xmachiei@gmail.com>
2016-03-21 14:59:48 +0100

    zad2 - done

--- lab4/zad2.js
+++ lab4/zad2.js
@@ -1,1 +1,10 @@
 /*jshint globalstrict: true, devel: true, esversion: 6 */
+
+String.prototype.nbsp = function() {
+	return this.replace(/(\s[aiouwz])\s/g, function(match, p1, offset, string) {
+		return p1 + '&nbsp;';
+	});
+};
+
+var tekst = 'Ala i As poszli w las';
+console.log(tekst.nbsp());
